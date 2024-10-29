4f4f046b0fd47410f031dc4b201bab577b5f4017
Alina Luchytska <aluchitska@gmail.com>
2024-10-29 20:50:21 +0200

    make build

---
+++ index.6b3210e7.js
@@ -0,0 +1,2 @@
+var e=document.getElementById("largeImg");document.querySelector("ul").addEventListener("click",function(t){t.preventDefault();var r=t.target;"IMG"===r.tagName&&(r=r.parentElement),e.src=r.href});
+//# sourceMappingURL=index.6b3210e7.js.map
